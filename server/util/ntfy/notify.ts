acaf41cb150925348fecfc4f0ca35d3c87bf808d
Juck <juckz@foxmail.com>
2023-09-25 18:28:13 +0800

    feat

--- server/util/ntfy/notify.ts
+++ server/util/ntfy/notify.ts
@@ -1,13 +1,10 @@
-// import ntfyWasmUrl from '@/utils/ntfy/ntfy.wasm?url';
-
+import fs from 'node:fs'
 
 export const loadNtfy = async () => {
     try {
         await import('./wasm_exec.js');
         const go = new Go();
-        const response = await fetch('http://localhost:3000/ntfy.wasm');
-        const wasmBuffer = await response.arrayBuffer();
-        const { instance } = await WebAssembly.instantiate(wasmBuffer, go.importObject);
+        const { instance } = await WebAssembly.instantiate(fs.readFileSync('public/ntfy.wasm'), go.importObject);
         await go.run(instance);
     } catch (error) {
         console.error('Error loading WASM:', error);
