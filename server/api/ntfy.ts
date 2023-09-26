bb417ac13df85fdad5837af2278746b765f84198
Juck <juckz@foxmail.com>
2023-09-26 15:43:19 +0800

    feat: 1.ntfy

--- server/api/ntfy.ts
+++ server/api/ntfy.ts
@@ -8,30 +8,40 @@ import wasmExec from './ntfy/wasm_exec.js';
 
 wasmExec()
 
-let res: any = undefined
-let loadNtfyTask = () => new Promise(async (resolve, reject) => {
+let goStatus = 'beforeload'
+let loadNtfyTask = new Promise(async (resolve, reject) => {
   try {
+    goStatus = 'loading'
     const go = new Go();
     // const ins = await WebAssembly.instantiate(fs.readFileSync(path.join(process.cwd(), 'public', 'ntfy.wasm')), go.importObject);
     const ins = await WebAssembly.instantiateStreaming(fetch("https://xiaoai.ihave.cool/ntfy.wasm"), go.importObject)
     const { instance } = ins;
     go.run(instance);
-    res = globalThis.hello('test')
-    resolve(res)
+    resolve(true)
   } catch (error) {
-    res = error
-    resolve(error)
+    reject(error)
   }
+})
+
+loadNtfyTask.then(res => {
+  goStatus = 'loaded'
+}).catch(e => {
+  console.error(e);
+  goStatus = 'error'
 })
 
 export default defineEventHandler(async (e) => {
   // const file = path.join(process.cwd(), 'files', 'test.json');
   // const stringified = fs.readFileSync(file, 'utf8');
-  const { name = 'world', pathval = './' } = getQuery(e)
-  await loadNtfyTask()
-  return {
-    res,
-    path: pathval,
-    test: globalThis.hello(name),
+  const query = getQuery(e)
+  const { name } = query
+  if (goStatus === 'loaded') {
+    return {
+      test: globalThis.hello(name),
+    }
+  } else {
+    return {
+      goStatus
+    }
   }
 })
