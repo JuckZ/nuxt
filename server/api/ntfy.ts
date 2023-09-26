84068e90c30f077b5363ab667eaae6540ccdca56
Juck <juckz@foxmail.com>
2023-09-26 14:29:33 +0800

    feat

--- server/api/ntfy.ts
+++ server/api/ntfy.ts
@@ -4,9 +4,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const file = path.join(process.cwd(), 'files', 'test.json');
-// const stringified = fs.readFileSync(file, 'utf8');
-
 let loadNtfyTask = (name: string) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -23,13 +20,18 @@ let loadNtfyTask = (name: string) => {
 }
 
 export default defineEventHandler(async (e) => {
-  const { name = 'world' } = getQuery(e)
-  // let res = await loadNtfyTask(name)
+  const { name = 'world', pathval = './' } = getQuery(e)
+  const file = path.join(process.cwd(), 'files', 'test.json');
+  const stringified = fs.readFileSync(file, 'utf8');
+  let res = await loadNtfyTask(name)
   return {
-    // res,
+    res,
     cwd: process.cwd(),
+    dir: fs.readdirSync(pathval),
+    dirname: __dirname,
+    dirDir: fs.readdirSync(__dirname),
     test: fs.readdirSync(process.cwd()),
+    filename: __filename
     // test: globalThis.hello('juck'),
-    path: [__filename, __dirname, fs.readdirSync(__dirname)]
   }
 })
