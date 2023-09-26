b4bfe218648a0d7295825d6986cf9d945e530b1b
Juck <juckz@foxmail.com>
2023-09-26 12:00:22 +0800

    test

--- server/api/ntfy.ts
+++ server/api/ntfy.ts
@@ -4,22 +4,33 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const loadNtfy = async () => {
-  try {
-    await import('./ntfy/wasm_exec.js');
-    const go = new Go();
-    const ins = await WebAssembly.instantiate(fs.readFileSync('server/wasm/ntfy.wasm'), go.importObject);
-    const { instance } = ins;
-    await go.run(instance);
-  } catch (error) {
-    console.error('Error loading WASM:', error);
-  }
-};
+let loadNtfyTask = (name: string) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await import('./ntfy/wasm_exec.js');
+      const go = new Go();
+      const ins = await WebAssembly.instantiate(fs.readFileSync('server/wasm/ntfy.wasm'), go.importObject);
+      const { instance } = ins;
+      go.run(instance);
+      resolve(globalThis.hello(name))
+    } catch (error) {
+      resolve(error)
+    }
+  })
+}
 
-loadNtfy()
+let timeoutTask = new Promise((resolve, reject) => {
+  let t1 = setTimeout(() => {
+    resolve('timeout')
+    clearTimeout(t1)
+  }, 2000);
+})
 export default defineEventHandler(async (e) => {
+  const { name = 'world' } = getQuery(e)
+  let res = await Promise.race([loadNtfyTask(name), timeoutTask])
   return {
-    test: globalThis.hello('juck'),
+    res,
+    // test: globalThis.hello('juck'),
     path: [__filename, __dirname, fs.readdirSync(__dirname)]
   }
 })
